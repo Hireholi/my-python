ede79f9e37495851c0401adeb92ef99a963e33c5
Hireholi <hireholi@gmail.com>
2019-12-29 12:53:52 +0530

    sum

---
+++ Sum.py
@@ -0,0 +1,5 @@
+#sum of two numbers
+num1 = input ("enter first no  :")
+num2 = input ("enter second no  :")
+sum = float(num1)+float(num2)
+print('sum of {0} and {1} is {2}'.format(num1, num2, sum))
